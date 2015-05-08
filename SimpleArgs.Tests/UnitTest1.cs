fd0823b9fdf19d06750b7bfbac9435510f09400f
rhyous <rhyous@yahoo.com>
2015-05-08 17:06:22 -0600

    Added unit tests

---
+++ SimpleArgs.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleArgs.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
