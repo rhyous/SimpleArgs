cc3a3ff496a2844b0c003b18f7d5e565f7489f39
rhyous <rhyous@yahoo.com>
2015-05-28 21:23:19 -0600

    Added Args.cs to more easily access Args statically.

---
+++ SimpleArgs/Business/Args.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleArgs
+{
+    public static class Args
+    {
+        public static string Value(string key)
+        {
+            return ArgumentList.Instance.Args[key].Value;
+        }
+
+        public static Argument Get(string key)
+        {
+            return ArgumentList.Instance.Args[key];
+        }
+    }
+}
