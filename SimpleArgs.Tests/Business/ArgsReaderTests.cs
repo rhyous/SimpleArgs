888ae1291b251d09e1be3b018b6f524b37b0a5d9
rhyous <rhyous@yahoo.com>
2015-07-22 18:54:09 -0600

    Brought back colon separated parameters. Added unit tests.

--- SimpleArgs.Tests/Business/ArgsReaderTests.cs
+++ SimpleArgs.Tests/Business/ArgsReaderTests.cs
@@ -7,6 +7,13 @@ namespace SimpleArgs.Tests.Business
     [TestClass]
     public class ArgsReaderTests
     {
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            ArgumentList.Instance.MessageBuilder = new FakeMessageBuilder();
+            ArgsHandlerCollection.Instance.Add(new ArgsHandler());
+        }
+
         public sealed class ArgsHandler : ArgsHandlerBase
         {
             public ArgsHandler()
@@ -20,6 +27,13 @@ namespace SimpleArgs.Tests.Business
                         Description = "This is an example of a file path parameter.",
                         Example = @"{name}=""C:\some\path\to\a\file.txt""",
                         DefaultValue = @"C:\path\to\a\file.txt",
+                    },
+                    new Argument
+                    {
+                        Name = "Param",
+                        ShortName = "p",
+                        Description = "This is an example parameter.",
+                        Example = @"{name}=value",
                     }
                     // Add more args here
                 };
@@ -35,13 +49,32 @@ namespace SimpleArgs.Tests.Business
         }
 
 
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestFilePathAsParameter()
         {
-            ArgumentList.Instance.MessageBuilder = new FakeMessageBuilder();
-            ArgsHandlerCollection.Instance.Add(new ArgsHandler());
             IReadArgs argsReader = new ArgsReader(ArgumentList.Instance) { IgnoreUnknownParams = true };
             argsReader.ParseArgs(new[] { @"File=""C:\some\Awesome\File.txt""" });
+            const string expected = @"C:\some\Awesome\File.txt";
+            Assert.AreEqual(expected, Args.Value("File"));
+        }
+
+        [TestMethod]
+        public void TestFilePathAsParameterColon()
+        {
+            IReadArgs argsReader = new ArgsReader(ArgumentList.Instance) { IgnoreUnknownParams = true };
+            argsReader.ParseArgs(new[] { @"File:""C:\some\Awesome\File.txt""" });
+            const string expected = @"C:\some\Awesome\File.txt";
+            Assert.AreEqual(expected, Args.Value("File"));
+        }
+
+        [TestMethod]
+        public void TestEqualSignInParameter()
+        {
+            IReadArgs argsReader = new ArgsReader(ArgumentList.Instance) { IgnoreUnknownParams = true };
+            argsReader.ParseArgs(new[] { @"Param=""a=b""" });
+            const string expected = "a=b";
+            Assert.AreEqual(expected, Args.Value("Param"));
         }
     }
 }
