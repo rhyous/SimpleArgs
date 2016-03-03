749020c3c7648439459a2e300cfbf93488e62224
rhyous <rhyous@yahoo.com>
2016-03-03 00:25:34 -0700

    Fixed issues with NuGetPackages. Versioned file.

--- SimpleArgs/Properties/AssemblyInfo.cs
+++ SimpleArgs/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleArgs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library for making command line arguments easy.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rhyous Publishgin LLC")]
 [assembly: AssemblyProduct("SimpleArgs")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Jared Barneck (Rhyous) 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
