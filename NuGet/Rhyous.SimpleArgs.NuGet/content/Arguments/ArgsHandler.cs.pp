be7eb59a2a954cb60c7c398cd9a5314c4a28ffc1
rhyous <rhyous@yahoo.com>
2016-03-21 12:28:35 -0600

    ArgsHandler.cpp file was missing from source

---
+++ NuGet/Rhyous.SimpleArgs.NuGet/content/Arguments/ArgsHandler.cs.pp
@@ -0,0 +1,48 @@
+﻿using Rhyous.SimpleArgs;
+using System;
+using System.Collections.Generic;
+
+namespace $rootnamespace$.Arguments
+{
+    // Add this line of code to Main() in Program.cs
+    //
+    //   ArgsManager.Instance.Start(new ArgsHandler(), args);
+    //
+
+    /// <summary>
+    /// A class that implements IArgumentsHandler where command line
+    /// arguments are defined.
+    /// </summary>
+    public sealed class ArgsHandler : ArgsHandlerBase
+    {
+        public ArgsHandler()
+        {
+            Arguments = new List<Argument>
+            {
+                new Argument
+                {
+                    Name = "Echo",
+                    ShortName = "E",
+                    Description = "I echo to the console whater you put after Echo=",
+                    Example = "{name}=\"Hello, World!\"",
+                    Action = (value) => { Console.WriteLine(value); }
+                },
+                new Argument
+                {
+                    Name = "Value",
+                    ShortName = "V",
+                    Description = "This is an example argument with a default value.",
+                    Example = "{name}=NonDefaultValue",
+                    DefaultValue = "SomeDefaultValue"
+                }
+                // Add more args here
+            };
+        }
+
+        public override void HandleArgs(IReadArgs inArgsHandler)
+        {
+            Handled = true;
+            Console.WriteLine("I handled the args!!!");
+        }
+    }
+}
